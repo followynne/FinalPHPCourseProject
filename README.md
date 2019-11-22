1205c299f77f09e0519e1bcbec4c0ce44252076c
Matteo <matteo.gregoricchio@libero.it>
2019-11-22 11:29:46 +0100

    added sql db script

--- README.md
+++ README.md
@@ -1,40 +1,12 @@
-## SimpleMVC
+Realizzare un'applicazione PHP per la gestione di un quotidiano online con almeno:
+- una home con un estratto degli articoli del giorno
+- una pagina dell'articolo completo
+- una login per entrare nell'area di amministrazione
+- un sistema di gestione articoli con operazioni CRUD
+- una pagina di logout
 
-This is a simple MVC framework for PHP that uses a simple routing system and PSR-7 request messages passed in a controller.
-
-The routing system is based on a simple associative array as follows:
-
-```php
-use SimpleMVC\Controller;
-
-return [
-    'GET /' => Controller\Home::class, 
-];
-```
-
-All the controllers are mapped with an HTTP method and a URL path separated by a space character.
-
-All the controllers implement a `ControllerInterface` with only one function `execute($request)`, where `$request` is PSR-7 `ServerRequestInterface`, as follows:
-
-```php
-namespace SimpleMVC\Controller;
-
-use Psr\Http\Message\ServerRequestInterface;
-
-interface ControllerInterface
-{
-    public function execute(ServerRequestInterface $request);
-}
-```
-
-This project is basically a tutorial for beginners of the [Model-View-Controller](https://en.wikipedia.org/wiki/Model%E2%80%93view%E2%80%93controller) architecture.
-
-It has been used in some PHP programming classes of the [ITS ICT Piemonte](http://www.its-ictpiemonte.it/) school in Italy.
-
-**NOTE:** Since this is a tutorial project, the usage of this software in a production environment is discouraged.
-
-### Copyright
-
-The author of this software is [Enrico Zimuel](https://github.com/ezimuel/).
-
-This software is released under the [Apache License](/LICENSE), Version 2.0.
+- Il progetto deve utilizzare SIMPLEMVC
+- Gli articoli devono essere memorizzati in un database (MySQL o SQLite) utilizzando di PHP per l'accesso
+- Deve esserci almeno un test unitario con
+- I codici sorgenti devono essere su github
+- La consegna avviene tramite invio le .zip su moodle
---
+++ scriptdb.sql
@@ -0,0 +1,80 @@
+-- MySQL dump 10.13  Distrib 5.7.28, for Linux (x86_64)
+--
+-- Host: localhost    Database: FinalCourse
+-- ------------------------------------------------------
+-- Server version	5.7.28-0ubuntu0.18.04.4
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `articles`
+--
+
+DROP TABLE IF EXISTS `articles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `articles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `iduser` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `seotitle` varchar(255) NOT NULL,
+  `art_date` date DEFAULT NULL,
+  `content` text,
+  PRIMARY KEY (`id`),
+  KEY `iduser` (`iduser`),
+  CONSTRAINT `articles_ibfk_1` FOREIGN KEY (`iduser`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `articles`
+--
+
+LOCK TABLES `articles` WRITE;
+/*!40000 ALTER TABLE `articles` DISABLE KEYS */;
+/*!40000 ALTER TABLE `articles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `pwd` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-11-22 11:28:42
