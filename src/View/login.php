1043473645f6871f5adfe400bd25f79d9b69e28d
DaviCarbo <58367385+DaviCarbo@users.noreply.github.com>
2019-12-01 18:10:54 +0100

    Update login.php

--- src/View/login.php
+++ src/View/login.php
@@ -3,8 +3,8 @@
 <h1>Login</h1>
 <div>
     <form action="/login" method="POST" name="login">
-        <input type="email" name="user"><br/>
-        <input type="password" name="pwd"><br/>
+        <p>email</p><input type="email" name="user"><br/>
+        <p>password</p><input type="password" name="pwd"><br/>
         <input type="submit" name="login" >
     </form>
     <div>Se non sei registrato, <a href="/register">clicca qui per farlo</a></div>
